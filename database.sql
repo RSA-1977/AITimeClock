d84c19b4e8254cca09fe168a62c6390de5600f80
jm3495 <54543123+jm3495@users.noreply.github.com>
2024-04-18 14:42:39 -0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -6,13 +6,13 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    is_supervisor TINYINT(1) NOT NULL DEFAULT 0
+    is_supervisor BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE shifts (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    start_time DATETIME NOT NULL,
-    end_time DATETIME NOT NULL,
+    user_id INT,
+    start_time DATETIME,
+    end_time DATETIME,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
